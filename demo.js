ddfe48db2fea87a836aa0e4ff3e579c7f1017127
Shubham <shubham0209@gmail.com>
2017-09-29 15:12:17 +0530

    Removed setInterval

--- demo.js
+++ demo.js
@@ -3,11 +3,5 @@ var http = require("http");
 http.createServer(function(request, response){
 	response.writeHead(200, {"Content-Type": "text/html"});
 	var n = 0;
-	setInterval(function(){
-		response.write("<h1>Welcome to "+request.url+" "+n+" no. of times</h1>");
-		n++;
-		if(n >= 10){
-			return response.end();
-		}
-	}, 1000);
+	response.write("<h1>Welcome to "+request.url"</h1>");
 }).listen(8000);
