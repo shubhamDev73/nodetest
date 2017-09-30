10127f11778ff4256252bf22ba2a55c57afa0f49
Shubham <shubham0209@gmail.com>
2017-10-01 05:29:37 +0530

    Removed log stuff (but kept as comment)

--- demo.js
+++ demo.js
@@ -3,6 +3,5 @@ var http = require("http");
 http.createServer(function(request, response){
 	response.writeHead(200, {"Content-Type": "text/html"});
 	response.write("<h1>Welcome to "+request.url+"</h1>");
-	response.write("<h1>Welcome to "+(process.env.PORT || 5000)+"</h1>");
 	response.end();
-}).listen(process.env.PORT || 5000);
+}).listen(process.env.PORT || 5000);//24782
