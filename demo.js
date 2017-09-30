82e0ea109d95747a03c507cb102f26728ef6c0d7
Shubham <shubham0209@gmail.com>
2017-10-01 05:17:11 +0530

    Ended response

--- demo.js
+++ demo.js
@@ -2,6 +2,6 @@ var http = require("http");
 
 http.createServer(function(request, response){
 	response.writeHead(200, {"Content-Type": "text/html"});
-	var n = 0;
 	response.write("<h1>Welcome to "+request.url+"</h1>");
+	response.end();
 }).listen(8000);
