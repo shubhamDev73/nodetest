819f337a57f63906cb843aa97721fb31adcff849
Shubham <shubham0209@gmail.com>
2017-10-01 05:28:00 +0530

    Changed console stuff to browser stuff

--- demo.js
+++ demo.js
@@ -3,7 +3,6 @@ var http = require("http");
 http.createServer(function(request, response){
 	response.writeHead(200, {"Content-Type": "text/html"});
 	response.write("<h1>Welcome to "+request.url+"</h1>");
+	response.write("<h1>Welcome to "+(process.env.PORT || 5000)+"</h1>");
 	response.end();
 }).listen(process.env.PORT || 5000);
-
-console.log(process.env.PORT || 5000);
